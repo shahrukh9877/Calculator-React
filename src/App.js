4b6a47e75e5d2bc0b174a3728f46c5d9beca6bb1
shahrukh9877 <shahrukh.ahmad574@gmail.com>
2023-06-13 21:56:47 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './Button.css'
+import Main from './Main';
+function App() {
+  return (
+    <div className="app">
+  <div className='child'>
+
+      <Main/>
+  </div>
+    </div>
+  );
+}
+
+export default App;
